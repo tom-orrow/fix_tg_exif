9a270b7b1580ffb2ed91feeb216ee7a53e5027f1
Andrey Yuferov <asjuferov@gmail.com>
2023-02-17 03:15:28 +0700

    fixed icloud relative path

--- sort_icloud_media/main.py
+++ sort_icloud_media/main.py
@@ -6,11 +6,11 @@ import shutil
 from exif import Image
 from datetime import datetime
 
-
+DIR = os.path.dirname(__file__)
 FILE_EXT = ('jpg', 'jpeg', 'gif', 'JPG', 'JPEG', 'GIF', 'png', 'PNG')
 
 def modify_exif(filepath: str):
-    folder_path = 'garbage'
+    folder_path = f'{DIR}/garbage'
     filepath_wo_ext = re.findall(r'(.+?)\.[^\/]+$', filepath)[0]
     filename = re.findall(r'.+?\/([^\/]+)\..+$', filepath)[0]
     fileext = re.findall(r'.+?\/[^\/]+\.(.+)$', filepath)[0]
@@ -19,11 +19,11 @@ def modify_exif(filepath: str):
         img = Image(filepath)
         date = img.get('datetime_original')
         dt = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-        folder_path = f"output/{dt.strftime('%Y')}/{dt.strftime('%m')}"
+        folder_path = f"{DIR}/output/{dt.strftime('%Y')}/{dt.strftime('%m')}"
         os.makedirs(folder_path, exist_ok=True)
     except Exception as e:
         pass
-
+    
     shutil.copyfile(filepath, f'{folder_path}/{filename}.{fileext}')
     try:
         shutil.copyfile(f'{filepath_wo_ext}.MOV', f'{folder_path}/{filename}.MOV')
@@ -32,7 +32,7 @@ def modify_exif(filepath: str):
 
 def main():
     for ext in FILE_EXT:
-        for filename in glob.iglob(f'input/**/*.{ext}', recursive=True):
+        for filename in glob.iglob(f'{DIR}/input/**/*.{ext}', recursive=True):
             modify_exif(filename)
 
 
