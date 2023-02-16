34d3a04d4a331d2e9f75953057b8bcace5029909
Andrey Yuferov <asjuferov@gmail.com>
2023-02-16 21:21:40 +0700

    initial

---
+++ main.py
@@ -0,0 +1,34 @@
+import glob
+import re
+import os
+
+from exif import Image
+from datetime import datetime
+
+def modify_exif(filepath: str):
+    date = re.findall(r'.+?photo_.+?@([^\.]+)\.', filepath)[0]
+    filename = re.findall(r'.+?\/(photo_.+$)', filepath)[0]
+    dt = datetime.strptime(date, '%d-%m-%Y_%H-%M-%S')
+
+    folder_path = f"output/{dt.strftime('%Y')}/{dt.strftime('%m')}"
+    os.makedirs(folder_path, exist_ok=True)
+
+    img = Image(filepath)
+    exif_dt = dt.strftime('%Y:%m:%d %H:%M:%S')
+    img.DateTime = exif_dt
+    img.DateTimeOriginal = exif_dt
+    img.DateTimeDigitized = exif_dt
+
+    with open(f'{folder_path}/{filename}', 'wb') as f:
+        f.write(img.get_file())
+
+def main():
+    for filename in glob.iglob('input/**/photo_*@*.*', recursive=True):
+        if re.search(r'_thumb\..+$', filename):
+            continue
+        
+        modify_exif(filename)
+
+
+if __name__ == '__main__':
+    main()
