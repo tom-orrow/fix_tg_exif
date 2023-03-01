b99b4d2715a5fa2a2fdda92c15ed1ee2b14f4fa2
Andrey Yuferov <asjuferov@gmail.com>
2023-03-01 22:48:56 +0700

    new fixes

--- fix_tg_exif/exif_fix_from_name.py
+++ fix_tg_exif/exif_fix_from_name.py
@@ -8,21 +8,32 @@ from datetime import datetime
 DIR = os.path.dirname(__file__)
 
 def modify_exif(filepath: str):
-    date = '_'.join(re.findall(r'.*?IMG_(\d+)_(\d+)\.jpg', filepath)[0])
-    new_filename = re.findall(r'.*?(IMG_\d+_\d+\.jpg)', filepath)[0]
+    try:
+        date = '_'.join(re.findall(r'.*?IMG_(\d+)_(\d+).*?\.jpg', filepath)[0])
+    except:
+        return
+    
+    new_filename = re.findall(r'.*?(IMG_\d+_\d+.*?\.jpg)', filepath)[0]
     dt = datetime.strptime(date, '%Y%m%d_%H%M%S')
 
     folder_path = f"{DIR}/output/{dt.strftime('%Y')}/{dt.strftime('%m')}"
-    os.makedirs(folder_path, exist_ok=True)
-
+    
     img = Image(filepath)
     exif_dt = dt.strftime('%Y:%m:%d %H:%M:%S')
-    img.DateTime = exif_dt
-    img.DateTimeOriginal = exif_dt
-    img.DateTimeDigitized = exif_dt
+    dt_orig = None
+    try:
+        dt_orig = img.get('datetime_original')
+    except:
+        pass
     
-    with open(f'{folder_path}/{new_filename}', 'wb') as f:
-        f.write(img.get_file())
+    if dt_orig is None or dt_orig.split()[0] != datetime.strftime(dt, '%Y:%m:%d'):
+        os.makedirs(folder_path, exist_ok=True)
+        img.DateTime = exif_dt
+        img.DateTimeOriginal = exif_dt
+        img.DateTimeDigitized = exif_dt
+    
+        with open(f'{folder_path}/{new_filename}', 'wb') as f:
+            f.write(img.get_file())
 
 def main():
     for filename in glob.iglob(f'{DIR}/input/**/IMG_*_*.jpg', recursive=True):
